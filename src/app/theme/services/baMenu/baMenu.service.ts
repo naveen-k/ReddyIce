fed1fa1bb845b294ffc4e335edb82e1b92893f9e
Alok Kumar <kumaral@HCL.COM>
2017-08-17 14:51:07 +0530

    menu selection issues

--- src/app/theme/services/baMenu/baMenu.service.ts
+++ src/app/theme/services/baMenu/baMenu.service.ts
@@ -121,7 +121,7 @@ export class BaMenuService {
   }
 
   protected _selectItem(object:any):any {
-    object.selected = this._router.isActive(this._router.createUrlTree(object.route.paths), object.pathMatch === 'full');
+    object.selected = this._router.isActive(this._router.createUrlTree(object.route.paths), object.pathMatch === 'prefix');
     return object;
   }
 }
