24ef28f276eb756ae9031ae9b76889956af3aa0f
My Macbook Pro <shakir.h@hcl.com>
2017-09-12 01:37:55 +0530

    create user form redesign

--- src/app/pages/day-end/components/day-end-details/details.component.ts
+++ src/app/pages/day-end/components/day-end-details/details.component.ts
@@ -25,6 +25,7 @@ export class DetailsComponent implements OnInit {
     Coins: any;
     Cash: any;
     Misc: any;
+    disabled:boolean = false;
     tripDate: any = '2017-08-31';
     unitReconciliation: any = [];
     logedInUser:any = {};
@@ -36,7 +37,7 @@ export class DetailsComponent implements OnInit {
         ActulaCoin: 0.0,
         Tolls: 0.0,
         Misc: 0.0,
-        TtripStatusID: 1.0
+        TripStatusID: 1.0
     };
 
 
@@ -86,7 +87,9 @@ export class DetailsComponent implements OnInit {
             });
 
         this.tripDate = this.tripData.Created.split('T')[0];
-        this.reconciliationDTO.TtripStatusID = this.tripData.TtripStatusID;
+        this.reconciliationDTO.TripStatusID = this.tripData.TripStatusID;
+        if(this.tripData.TripStatusID == 24 || this.tripData.TripStatusID == 25) { this.disabled = true; };
+       console.log(this.disabled);
         this.service.getTripDetailByDate(this.tripID, this.tripDate).subscribe((res) => {
             this.ticketDetails = res;
             console.log(res);
@@ -116,6 +119,7 @@ export class DetailsComponent implements OnInit {
                     element['ReturnQuantity'] = element['ReturnQuantity'] || element['Returns'];
                     element['DamageQuantity'] = element['DamageQuantity'] || element['TruckDamage'];
                     element['CustomerDamageDRV'] = element['CustomerDamageDRV'] || element['CustomerDamage'];
+                    this.unitReconChange(element);
                 });
             }
             // this.mapUnitReconData(res);
