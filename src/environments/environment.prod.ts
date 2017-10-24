6a3ef1c3c76dd37074811b464f6be4cb533e2aca
Alok Kumar <kumaral@HCL.COM>
2017-10-24 20:33:36 +0530

    api path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
+// UAT: http://frozen.reddyice.com/reddyice_dev/
+// DEV: http://frozen.reddyice.com/myicebox_dev2/
+
 export const environment = {
   production: true,
-  apiEndpoint: 'http://frozen.reddyice.com/multiplebranch/',
+  apiEndpoint: 'http://frozen.reddyice.com/myicebox_dev2/',
   reportEndpoint: 'http://frozen.reddyice.com/DashboardReports/Reports/ReportData.aspx',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,11 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-// 'http://frozen.reddyice.com/myiceboxservice_dev/'
-// API http://frozen.reddyice.com/reddyice_dev/
+// UAT: http://frozen.reddyice.com/reddyice_dev/
+// DEV: http://frozen.reddyice.com/myicebox_dev2/
+
 export const environment = {
   production: false,
-  apiEndpoint: 'http://frozen.reddyice.com/apiicebox/',
+  apiEndpoint: 'http://frozen.reddyice.com/myicebox_dev2/',
   reportEndpoint: 'http://frozen.reddyice.com/DashboardReports/Reports/ReportData.aspx',
 };
