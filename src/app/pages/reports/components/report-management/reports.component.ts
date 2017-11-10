c48c14df15a9b163df99a0c72f250eb6be775982
Adhiraj Singh <adhiraj.s@HCL.COM>
2017-11-10 20:39:56 +0530

    customer address

--- src/app/pages/reports/components/report-management/reports.component.ts
+++ src/app/pages/reports/components/report-management/reports.component.ts
@@ -265,7 +265,7 @@ export class ReportsComponent implements OnInit {
             const custID = this.filter.custID;
             if (rType === 'DR') {
                 this.linkRpt = this.sanitizer.bypassSecurityTrustResourceUrl
-                    (environment.reportEndpoint + `?Rtype=${this.filter.reportType}&StartDate=${this.formatDate(this.filter.startDate)}&EndDate=${this.formatDate(this.filter.endDate)}&IsRI=true&BranchID=${this.filter.branch === 1 ? 0 : this.filter.branch}&DistributorID=${this.filter.distributor === 1 ? 0 : this.filter.distributor}&DriverID=${this.filter.driver === 1 ? 0 : this.filter.driver}&LoggedInUserID=${this.user.UserId}&CustType=${this.filter.custType}&CustomerID=${custID}`);
+                    (environment.reportEndpoint + `?Rtype=${this.filter.reportType}&StartDate=${this.formatDate(this.filter.startDate)}&EndDate=${this.formatDate(this.filter.endDate)}&IsRI=true&BranchID=${this.filter.branch === 1 ? 0 : this.filter.branch}&DistributorID=${this.filter.distributor === 1 ? 0 : this.filter.distributor}&DriverID=${this.filter.driver === 1 ? 0 : this.filter.driver}&LoggedInUserID=${this.user.UserId}&CustType=${this.selectedCustomerType}&CustomerID=${custID}`);
             } else if (rType === 'RS') {
 
                 this.linkRpt = this.sanitizer.bypassSecurityTrustResourceUrl
