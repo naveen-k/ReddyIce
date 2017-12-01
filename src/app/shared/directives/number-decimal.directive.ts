6fabcb398d73ca2c9c88bc40b5e8773734ede17a
Rathnakar Thipireddy <rathnakar.t@HCL.COM>
2017-12-01 12:27:00 +0530

    number-decimal.directive rmv extra comment

--- src/app/shared/directives/number-decimal.directive.ts
+++ src/app/shared/directives/number-decimal.directive.ts
@@ -1,64 +1,3 @@
-// import { Directive, ElementRef, Input } from '@angular/core';
-// @Directive({
-//     selector: '[numberDecimal]',
-// })
-// export class NumberDecimalDirective {
-
-//     @Input()
-//     numberDecimal: boolean = false;
-
-//     constructor(el: ElementRef) {
-//         el.nativeElement.addEventListener('keypress', (e) => {
-//             let value = el.nativeElement.value;  
-//             value = value.replace(/[^0-9\.]/g, '')  
-//             let findsDot = new RegExp(/\./g)  
-//             let containsDot = value.match(findsDot);
-
-//             if (containsDot != null && ([46, 110, 190].indexOf(e.which) > -1)) {  
-//                 event.preventDefault();  
-//                 return false;  
-//             } 
-
-//             // let reg_two_decimal= new RegExp('^\d+(\.\d{0,2})?$');
-//             // if(!reg_two_decimal.test(value)) {
-//             //     event.preventDefault();  
-//             //     return false;  
-//             // }
-
-//             const keyCode = (e.keyCode ? e.keyCode : e.which);
-//             if ((keyCode > 47 && keyCode < 58) || (keyCode === 8)) {
-//                 return true;
-//             }
-//             else if (this.numberDecimal && keyCode === 46) { return true } // accept decimal value 
-//             e.preventDefault();
-//         });
-//         el.nativeElement.addEventListener('focus', (e) => {
-//             let value = el.nativeElement.value;  
-//             if(value == '0.00'){
-//                 el.nativeElement.value ='';
-//             }
-//         });
-//         el.nativeElement.addEventListener('focusout', (e) => {
-//             let value = el.nativeElement.value;  
-//             if(value == '' || value == '.'){
-//                 el.nativeElement.value ='0.00';
-//             } else {
-//                 var result = Number(value);
-//                 var res = value.split(".");
-//                 if(value.indexOf('.') === -1) {
-//                     let r = result.toFixed(2);
-//                     value = r.toString();
-//                 } else if (res[1].length < 3) {
-//                     let r = result.toFixed(2);
-//                     value = r.toString();
-//                 }
-//                 el.nativeElement.value = value;              
-//             }
-//         });
-//     }
-// }
-
-
 import { Directive, HostListener, ElementRef, OnInit, EventEmitter, Output, Input } from "@angular/core";
 
 @Directive({ selector: "[numberDecimal]" })
@@ -84,12 +23,6 @@ export class NumberDecimalDirective implements OnInit {
                 return false;  
             } 
 
-            // let reg_two_decimal= new RegExp('^\d+(\.\d{0,2})?$');
-            // if(!reg_two_decimal.test(value)) {
-            //     event.preventDefault();  
-            //     return false;  
-            // }
-
             const keyCode = (e.keyCode ? e.keyCode : e.which);
             if ((keyCode > 47 && keyCode < 58) || (keyCode === 8)) {
                 return true;
@@ -104,33 +37,16 @@ export class NumberDecimalDirective implements OnInit {
   }
 
 //   @HostListener("keypress", ["$event.target.value"])
-//   keypress(value) {console.log(value,'aaaaaaaaa',this.el,this.el.which);
-//         // value = value.replace(/[^0-9\.]/g, '')  
-//         // let findsDot = new RegExp(/\./g)  
-//         // let containsDot = value.match(findsDot);
-
-//         // if (containsDot != null && ([46, 110, 190].indexOf(e.which) > -1)) {  
-//         //     event.preventDefault();  
-//         //     return false;  
-//         // } 
-
-//         // // let reg_two_decimal= new RegExp('^\d+(\.\d{0,2})?$');
-//         // // if(!reg_two_decimal.test(value)) {
-//         // //     event.preventDefault();  
-//         // //     return false;  
-//         // // }
-
-//         // const keyCode = (e.keyCode ? e.keyCode : e.which);
-//         // if ((keyCode > 47 && keyCode < 58) || (keyCode === 8)) {
-//         //     return true;
-//         // }
-//         // else if (this.numberDecimal && keyCode === 46) { return true } // accept decimal value 
-//         // e.preventDefault();     
-//   }
+//   keypress(value) {console.log('come')
+//         let number = value.split('.');
+//         if(number[1] && number[1].length > 1) {
+//             this.ngModelChange.emit((parseFloat(value)).toFixed(2));
+//         }            
+//   }  
 
   @HostListener("focus", ["$event.target.value"])
   onFocus(value) {
-        if(value == '0.00'){
+        if((parseFloat(value)).toFixed(2) == '0.00'){
             this.el.value = '';
             this.ngModelChange.emit(this.el.value);
         }      
@@ -155,15 +71,4 @@ export class NumberDecimalDirective implements OnInit {
                 this.ngModelChange.emit(this.el.value);             
             }     
   }
-
-//   @HostListener("blur", ["$event.target.value"])
-//   onBlur(value) {
-//     this.el.value = this.cleanNumber(value); //"987654" ;//this.currencyPipe.transform(value);
-//     this.ngModelChange.emit(this.el.value);
-//   }
-
-//   cleanNumber (value: number) {
-//     return 8888888;
-//   }
-
 }
