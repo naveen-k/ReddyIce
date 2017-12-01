0832d8c75c9e46e823433a94db3a9c0c00d3b653
Rathnakar Thipireddy <rathnakar.t@HCL.COM>
2017-12-01 16:50:29 +0530

    update numbers only directive by ashish

--- src/app/shared/directives/number-only.directive.ts
+++ src/app/shared/directives/number-only.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, HostListener, ElementRef, OnInit, EventEmitter, Output, Input } from "@angular/core";
 @Directive({
     selector: '[numberOnly]',
 })
 export class NumberOnlyDirective {
-
+    private el: any;
     @Input()
     numberOnly: boolean = false;
+    @Output() ngModelChange = new EventEmitter();
 
-    constructor(el: ElementRef) {
-        el.nativeElement.addEventListener('keypress', (e) => {
+    constructor(private elementRef: ElementRef) {
+        this.el = this.elementRef.nativeElement;
+        this.el.addEventListener('keypress', (e) => {
             const keyCode = (e.keyCode ? e.keyCode : e.which);
             if (this.numberOnly && keyCode === 46 && e.target.value.indexOf(".") < 0) { // check for decimal
                 return true;
@@ -18,11 +20,54 @@ export class NumberOnlyDirective {
             }
             e.preventDefault();
         });
-
-        el.nativeElement.addEventListener('keyup', (e) => {
-            if (this.numberOnly && e.target.value.indexOf(".") >= 0 && e.target.value.split(".")[1].length > 2) {
-                e.target.value = e.target.value.substring(0, e.target.value.indexOf(".") + 3);
-            }
-        })
     }
+
+    ngOnInit() {
+        this.el.value = this.el.value;
+      }
+
+      @HostListener("keyup", ["$event.target.value"])
+      keyup(value) {
+            if(!this.numberOnly){
+                return;
+            }
+            if (this.numberOnly && value.indexOf(".") >= 0 && value.split(".")[1].length > 2) {
+                let result = value.substring(0, value.indexOf(".") + 3);
+                this.ngModelChange.emit(result);
+            }   
+      }      
+      
+      @HostListener("focus", ["$event.target.value"])
+      onFocus(value) {
+            if(!this.numberOnly){
+                return;
+            }
+            if((parseFloat(value)).toFixed(2) == '0.00'){
+                this.el.value = '';
+                this.ngModelChange.emit(this.el.value);
+            }      
+      }
+    
+      @HostListener("focusout", ["$event.target.value"])
+      focusout(value) {
+                if(!this.numberOnly){
+                    return;
+                }          
+                if(value == '' || value == '.'){
+                    this.el.value = '0.00';
+                    this.ngModelChange.emit(this.el.value);  
+                } else {
+                    var result = Number(value);
+                    var res = value.split(".");
+                    if(value.indexOf('.') === -1) {
+                        let r = result.toFixed(2);
+                        value = r.toString();
+                    } else if (res[1].length < 3) {
+                        let r = result.toFixed(2);
+                        value = r.toString();
+                    }
+                    this.el.value = value;   
+                    this.ngModelChange.emit(this.el.value);             
+                }     
+      }      
 }
