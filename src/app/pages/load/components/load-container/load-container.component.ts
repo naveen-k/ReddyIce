157bdcc46ea01de86c9ecd08b97d187632b5a6d3
Priyanka Sharma <priyanka_sh@hcl.com>
2017-12-07 17:09:33 +0530

    loader internal redirection

--- src/app/pages/load/components/load-container/load-container.component.ts
+++ src/app/pages/load/components/load-container/load-container.component.ts
@@ -1,11 +1,20 @@
 import { UserService } from '../../../../shared/user.service';
 import { LoadService } from '../../load.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { NotificationsService } from 'angular2-notifications';
 @Component({
     template: `<router-outlet></router-outlet>`,
 })
 export class LoadContainerComponent {
-    constructor(service: LoadService, user: UserService) {
+    constructor(service: LoadService, user: UserService,
+        protected activatedRoute: ActivatedRoute,protected router: Router,
+        protected notification: NotificationsService) {
+           
+        if(!user.getUser().IsRIInternal){
+            this.notification.error('', 'You are not authorized to access this module.');
+            this.router.navigate(['/pages']);
+        }
         let filter = service.getFilter();
         const now = new Date();
         
