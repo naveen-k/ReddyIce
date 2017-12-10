780f4abd5abb3c5389948fa77f47f908de98f607
Priyanka Sharma <priyanka_sh@hcl.com>
2017-12-10 16:29:08 +0530

    update branch in user management by ashish singla

--- src/app/shared/components/multiple-select/multiple-select.ts
+++ src/app/shared/components/multiple-select/multiple-select.ts
@@ -75,6 +75,7 @@ export class SelectComponent implements AfterViewInit {
             position: this.position,
             selectAll: this.selectAll,
             allSelected: false,
+            onClick: this.onClose.bind(this)
         });
     }
 
