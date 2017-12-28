11075822667ce9f421aab594846b987ceafe3a85
Rishabh Rastogi <rishabh_r@HCL.COM>
2017-12-28 15:29:35 +0530

    prod LABEL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,6 +15,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'http://frozen.reddyice.com/iceboxapisqldev/',
-  reportEndpoint: 'http://frozen.reddyice.com/iceboxReportSqldev/Reports/ReportData.aspx'
+  apiEndpoint: 'http://frozen.reddyice.com/reddyice_dev/',
+  reportEndpoint: 'http://frozen.reddyice.com/DashboardReports/Reports/ReportData.aspx',
+  prodLabel: '(BUAT)'
 };
