57d978153ef371ed550b891881203c1a68be2fb7
Ashish Singla <ashish.s@hcl.com>
2018-01-24 19:28:48 +0530

    edi combo name ticket fixed

--- src/app/pages/manual-tickets/ticket-list/ticket-list.component.ts
+++ src/app/pages/manual-tickets/ticket-list/ticket-list.component.ts
@@ -237,7 +237,7 @@ export class TicketListComponent implements OnInit {
             this.allFilterdTickets = this.getFilteredAllTicket();
             this.unSelectAll();
             this.allTickets.forEach(element => {
-                element['ticketType'] = this.service.getTicketType(element.IsSaleTicket, element.Customer, element.TicketTypeIDelement, element.CustomerTypeID, element.UserName ? (element.UserName.replace(/\s/g, "").replace(/-+/g, '') == this.EDIUserName) : false)
+                element['ticketType'] = this.service.getTicketType(element.IsSaleTicket, element.Customer, element.TicketTypeID, element.CustomerTypeID, element.UserName ? (element.UserName.replace(/\s/g, "").replace(/-+/g, '') == this.EDIUserName) : false)
             });
             this.showSpinner = false;
 
