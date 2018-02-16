f4246e90e34b23062038f2357b26f1fc98b027b5
Rishabh Rastogi <rishabh_r@HCL.COM>
2018-02-16 13:42:41 +0530

    some changes

--- src/app/shared/components/multiple-select/multiple-select.ts
+++ src/app/shared/components/multiple-select/multiple-select.ts
@@ -74,7 +74,7 @@ export class SelectComponent implements AfterViewInit {
             position: this.position,
             selectAll: this.selectAll,
             allSelected: false,
-            onClick: (this.multiple) ? this.onClose.bind(this) : void (0)
+            onClick: this.onClose.bind(this)//(this.multiple) ? this.onClose.bind(this) : void (0)
         });
     }
 
