2bdebd54ba93f62f419fe9bbad72ee1f23c780b3
Pritika Agrawal <pritika.a@HCL.COM>
2018-08-08 17:38:26 +0530

    vertex change for customer screen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://buat2-myicebox.reddyice.com/MyIceBoxApi/',
+//apiEndpoint: 'https://buat2-myicebox.reddyice.com/MyIceBoxApi/',
+	apiEndpoint: 'http://dev-myicebox.reddyice.com/VertaxApi/',
     reportEndpoint: 'https://buat2-myicebox.reddyice.com/MyIceBoxReports/Reports/ReportData.aspx',
     inventoryEndpoint: 'https://buat2-myicebox.reddyice.com/DA_Inventory/forms/addBom.aspx',
     prodLabel: '(Performance)',
