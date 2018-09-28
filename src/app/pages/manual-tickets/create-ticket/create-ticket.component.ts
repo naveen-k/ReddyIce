5036dc068d89f858999d3b2c0a29215f3e846f8e
Pritika Agrawal <pritika.a@HCL.COM>
2018-09-28 15:09:48 +0530

    decimal issue

--- src/app/pages/manual-tickets/create-ticket/create-ticket.component.ts
+++ src/app/pages/manual-tickets/create-ticket/create-ticket.component.ts
@@ -1255,12 +1255,14 @@ aftersuccessfulSubmit(){
     this.ticket.TotalSale = 0;
     this.tempModels.totalTax = 0;
 	var multiplier = Math.pow(10, 2);
+	var multiplier_line_item_tax = Math.pow(10, 5);
     this.ticket.TicketProduct.forEach((t) => {
 	
       this.ticket['tempTotalUnit'] += +t.Quantity || 0;
 	  t['totalAmount'] = Math.round(t['totalAmount'] * multiplier) / multiplier;
 	   this.ticket.TotalSale = +this.ticket.TotalSale.fpArithmetic("+", +t['totalAmount'] || 0);
 	  var line_item_tax = t['totalAmount'].fpArithmetic("*", t.TaxPercentage) / 100;
+	  line_item_tax = Math.round(line_item_tax * multiplier_line_item_tax) / multiplier_line_item_tax;
       this.tempModels.totalTax = +this.tempModels.totalTax.fpArithmetic("+", (+line_item_tax));
     });
     
